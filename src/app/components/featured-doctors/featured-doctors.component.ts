9d87e05c4c40c0f089ac7a732fe7870a2f4ffb18
Abanobshafeeq <Abanoub20010@feng.bu.edu.eg>
2025-12-28 16:20:55 +0200

    edit featurd doctors

--- src/app/components/featured-doctors/featured-doctors.component.ts
+++ src/app/components/featured-doctors/featured-doctors.component.ts
@@ -7,62 +7,65 @@ import { Doctor } from '../../models/doctor';
 @Component({
   selector: 'app-featured-doctors',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './featured-doctors.component.html',
-  styleUrls: ['./featured-doctors.component.scss']
+  styleUrls: ['./featured-doctors.component.scss'],
 })
 export class FeaturedDoctorsComponent implements OnInit {
-
   specialties = SPECIALTIES;
   doctors: Doctor[] = DOCTORS;
   filteredDoctors: Doctor[] = [];
   activeSpecialty = 0;
 
-  row1Doctors: Doctor[] = [];
-  row2Doctors: Doctor[] = [];
+  doctorPairs: Doctor[][] = [];
 
   ngOnInit() {
     this.filteredDoctors = this.doctors;
-    this.splitRows();          
-  }
-
-  swiperBreakpoints = {
-    1200: {
-      slidesPerView: 6.5 ,      slidesPerGroup: 6.5
-    },
-    992: {
-      slidesPerView: 5,
-      slidesPerGroup: 5
-    },
-    768: {
-      slidesPerView: 4,
-      slidesPerGroup: 4
-    },
-    576: {
-      slidesPerView: 3,
-      slidesPerGroup: 3
-    },
-    0: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    }
-  };
-
-  private splitRows() {
-    this.row1Doctors = this.filteredDoctors.filter(d => d.id % 2 === 1);
-    this.row2Doctors = this.filteredDoctors.filter(d => d.id % 2 === 0);
+    this.buildPairs();
   }
 
   filter(id: number) {
     this.activeSpecialty = id;
+
     this.filteredDoctors =
       id === 0
         ? this.doctors
-        : this.doctors.filter(d => d.specialtyId === id);
+        : this.doctors.filter((d) => d.specialtyId === id);
 
-    this.splitRows();        
+    this.buildPairs();
   }
+
+  private buildPairs() {
+    this.doctorPairs = [];
+    for (let i = 0; i < this.filteredDoctors.length; i += 2) {
+      this.doctorPairs.push([
+        this.filteredDoctors[i],
+        this.filteredDoctors[i + 1],
+      ]);
+    }
+  }
+
+  swiperBreakpoints = {
+    1200: {
+      slidesPerView: 6.5,
+      slidesPerGroup: 6.5,
+    },
+    992: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+    },
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    576: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    0: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  };
 }
