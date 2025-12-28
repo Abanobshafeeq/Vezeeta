ba42e6ff2a156793225a7bbb209486fedff2e899
Abanobshafeeq <Abanoub20010@feng.bu.edu.eg>
2025-12-28 16:12:15 +0200

    edit all of notes

--- src/app/components/featured-entities/featured-entities.component.ts
+++ src/app/components/featured-entities/featured-entities.component.ts
@@ -13,47 +13,30 @@ import { FEATURED_ENTITIES, Entity } from '../../data/entitiy.data';
 export class FeaturedEntitiesComponent implements AfterViewInit {
   entities: Entity[] = FEATURED_ENTITIES;
 
-  @ViewChild('swiperEl', { static: true }) swiperEl!: ElementRef;
-
-  swiperConfig = {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: false,
-    },
-    loopFillGroupWithBlank: true,
-    breakpoints: {
-      0: {
-        slidesPerView: 1.2,
-        slidesPerGroup: 1,
-        spaceBetween: 12,
-      },
-      480: {
-        slidesPerView: 1.6,
-        slidesPerGroup: 1,
-        spaceBetween: 12,
-      },
-      640: {
-        slidesPerView: 2.2,
-        slidesPerGroup: 2,
-        spaceBetween: 14,
-      },
-      768: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 16,
-      },
-      1024: {
-        slidesPerView: 4,
-        slidesPerGroup: 4,
-        spaceBetween: 16,
-      },
-    },
-  };
+  @ViewChild('swiperEl', { static: false }) swiperEl!: ElementRef;
 
   ngAfterViewInit(): void {
-    Object.assign(this.swiperEl.nativeElement, this.swiperConfig);
-    this.swiperEl.nativeElement.initialize();
+    setTimeout(() => {
+      const swiperEl = this.swiperEl.nativeElement;
+      
+      const swiperParams = {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+          delay: 1000,
+          disableOnInteraction: false,
+        },
+        breakpoints: {
+          0: { slidesPerView: 1.2, slidesPerGroup: 1, spaceBetween: 12 },
+          480: { slidesPerView: 1.6, slidesPerGroup: 1, spaceBetween: 12 },
+          640: { slidesPerView: 2.2, slidesPerGroup: 2, spaceBetween: 14 },
+          768: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 16 },
+          1024: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 16 },
+        },
+      };
+      
+      Object.assign(swiperEl, swiperParams);
+      swiperEl.initialize();
+    }, 150);
   }
 }
