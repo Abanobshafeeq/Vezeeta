244a9f1ebcd6572bcd52c8961da88d5efc2cf924
Abanobshafeeq <Abanoub20010@feng.bu.edu.eg>
2025-12-29 16:09:56 +0200

    solve all warning

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
--- src/app/components/service-card/service-card.component.ts
+++ src/app/components/service-card/service-card.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-card',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css'
 })
